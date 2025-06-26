f1a73ea8b378edf734bfbf54cacd45f6f092d2e7
Senuka Chandunu <senukaacc@gmail.com>
2025-06-26 22:33:58 +0530

    add clean footer

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    if (burger && nav && navLinks) {
+        burger.addEventListener('click', () => {
+            // Toggle Nav
+            nav.classList.toggle('nav-active');
+
+            // Animate Links
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = ''
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+                }
+            });
+            // Burger Animation
+            burger.classList.toggle('toggle');
+        });
+    }
+}
+
+navSlide();
+
 document.addEventListener('DOMContentLoaded', () => {
     const categoryCards = document.querySelectorAll('.category-card');
     const menuContainer = document.querySelector('.menu-container');
@@ -33,3 +59,14 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+@keyframes navLinkFade {
+    from {
+        opacity: 0;
+        transform: translateX(50px);
+    }
+    to {
+        opacity: 1;
+        transform: translateX(0px);
+    }
+}
